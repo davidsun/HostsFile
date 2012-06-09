95ed1533fc8cb9bb3b10f6c75d7763c8f578290c
David Sun <sunzheng.david@gmail.com>
2012-06-09 14:47:56 +0800

    Update algorithm

--- getHostsFile.py
+++ getHostsFile.py
@@ -1,31 +1,23 @@
 import os
 import sys
+import socket
 import threading
 
 from lib import synchronizedOutput, synchronizedQueue
 
 class myThread(threading.Thread) :
-    def __init__(self, name, queue, output) :
+    def __init__(self, queue, output) :
         threading.Thread.__init__(self)
-        self.name = name
         self.queue = queue
         self.output = output
-    def getFileName(self) :
-        return self.name + '.out'
     def run(self) :
         while True :
             cur = self.queue.get()
-            if (cur == None) :
-                break
-            os.system('ping -c 1 -W 5 ' + cur + ' > ' + self.getFileName())
-            fin = file(self.getFileName())
-            for l in fin.readlines() :
-                loc1 = l.find('(')
-                loc2 = l.find(')')
-                if (loc1 != -1 and loc2 != -1) :
-                    self.output.out(l[loc1 + 1 : loc2] + '\t' + cur)
-                break
-        os.system('rm ' + self.getFileName())
+            if (cur == None) : break
+            try :
+                result = socket.getaddrinfo(cur, None)
+                self.output.out(result[0][4][0] + '\t' + cur)
+            except : pass
 
 if len(sys.argv) != 2 :
     print sys.argv[0] + ' <Input File>'
@@ -37,7 +29,7 @@ else :
         q.put(line[ : -1])
     threads = list()
     for i in range(0, 5) :
-        t = myThread(i, q, out)
+        t = myThread(q, out)
         t.start()
         threads.append(t)
     for t in threads :
